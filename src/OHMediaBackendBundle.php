a20d9dffa6006f4b1424358b5a725757b97a122c
Justin Hoffman <justin@ohmedia.ca>
2024-03-25 15:32:19 -0600

    deprecation fixes

--- src/OHMediaBackendBundle.php
+++ src/OHMediaBackendBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OHMediaBackendBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
