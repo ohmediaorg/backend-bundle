338899577e103a0f4b722ad313c1b973a8833039
Justin Hoffman <justin@ohmedia.ca>
2024-03-25 17:11:44 -0600

    special class for button spacing

--- boilerplate/twig/index.tpl.php
+++ boilerplate/twig/index.tpl.php
@@ -35,20 +35,20 @@
           <td>
 <?php if ($has_view_route) { ?>
             {% if is_granted(attributes.view, <?php echo $singular['snake_case']; ?>) %}
-            <a class="btn btn-sm btn-primary" href="{{ path('<?php echo $singular['snake_case']; ?>_view', {id: <?php echo $singular['snake_case']; ?>.id}) }}" title="View">
+            <a class="btn btn-sm btn-primary btn-action" href="{{ path('<?php echo $singular['snake_case']; ?>_view', {id: <?php echo $singular['snake_case']; ?>.id}) }}" title="View">
               {{ bootstrap_icon('eye-fill') }}
               <span class="visually-hidden">View</span>
             </a>
             {% endif %}
 <?php } ?>
             {% if is_granted(attributes.edit, <?php echo $singular['snake_case']; ?>) %}
-            <a class="btn btn-sm btn-primary" href="{{ path('<?php echo $singular['snake_case']; ?>_edit', {id: <?php echo $singular['snake_case']; ?>.id}) }}" title="Edit">
+            <a class="btn btn-sm btn-primary btn-action" href="{{ path('<?php echo $singular['snake_case']; ?>_edit', {id: <?php echo $singular['snake_case']; ?>.id}) }}" title="Edit">
               {{ bootstrap_icon('pen-fill') }}
               <span class="visually-hidden">Edit</span>
             </a>
             {% endif %}
             {% if is_granted(attributes.delete, <?php echo $singular['snake_case']; ?>) %}
-            <a class="btn btn-sm btn-danger" href="{{ path('<?php echo $singular['snake_case']; ?>_delete', {id: <?php echo $singular['snake_case']; ?>.id}) }}" title="Delete" data-confirm="Are you sure you want to delete this <?php echo $singular['readable']; ?>? Clicking OK will take you to a verification step to delete this entry.">
+            <a class="btn btn-sm btn-danger btn-action" href="{{ path('<?php echo $singular['snake_case']; ?>_delete', {id: <?php echo $singular['snake_case']; ?>.id}) }}" title="Delete" data-confirm="Are you sure you want to delete this <?php echo $singular['readable']; ?>? Clicking OK will take you to a verification step to delete this entry.">
               {{ bootstrap_icon('trash-fill') }}
               <span class="visually-hidden">Delete</span>
             </a>
