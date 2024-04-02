58d076253bf5d3d2f5ff7fdf4412c279ae201574
Justin Hoffman <justin@ohmedia.ca>
2024-04-02 16:54:58 -0600

    missed class include

--- boilerplate/Entity.tpl.php
+++ boilerplate/Entity.tpl.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\<?php echo $singular['pascal_case']; ?>Repository;
+<?php if ($has_reorder) { ?>
+use Doctrine\DBAL\Types\Types;
+<?php } ?>
 use Doctrine\ORM\Mapping as ORM;
 use OHMedia\SecurityBundle\Entity\Traits\BlameableTrait;
 
