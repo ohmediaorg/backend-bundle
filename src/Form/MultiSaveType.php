4d3b3a2b01fa02e65b745f43341dd9a818f5e0e0
Justin Hoffman <justin@ohmedia.ca>
2025-11-28 13:19:13 -0600

    integrate a save and view button

--- src/Form/MultiSaveType.php
+++ src/Form/MultiSaveType.php
@@ -14,6 +14,10 @@ class MultiSaveType extends AbstractType
     {
         $builder->add('go_back', SubmitType::class);
 
+        if ($options['view']) {
+            $builder->add('view', SubmitType::class);
+        }
+
         $builder->add('keep_editing', SubmitType::class);
 
         if ($options['add_another']) {
@@ -30,6 +34,7 @@ class MultiSaveType extends AbstractType
             'row_attr' => [
                 'class' => 'fieldset-nostyle mb-3',
             ],
+            'view' => false,
             'add_another' => true,
         ]);
     }
