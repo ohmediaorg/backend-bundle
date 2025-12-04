c90675b329013e16531a6828858e0c8186c1f791
Justin Hoffman <justin@ohmedia.ca>
2025-12-04 10:52:49 -0600

    add email listing search

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -2,6 +2,7 @@
 
 namespace OHMedia\BackendBundle\Controller;
 
+use Doctrine\ORM\QueryBuilder;
 use OHMedia\BackendBundle\Routing\Attribute\Admin;
 use OHMedia\BackendBundle\Security\Voter\EmailVoter;
 use OHMedia\BootstrapBundle\Service\Paginator;
@@ -9,6 +10,10 @@ use OHMedia\EmailBundle\Entity\Email;
 use OHMedia\EmailBundle\Repository\EmailRepository;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +23,8 @@ class EmailController extends AbstractController
     #[Route('/emails', name: 'email_index', methods: ['GET'])]
     public function index(
         EmailRepository $emailRepository,
-        Paginator $paginator
+        Paginator $paginator,
+        Request $request,
     ): Response {
         $newEmail = new Email();
 
@@ -31,10 +37,60 @@ class EmailController extends AbstractController
         $qb = $emailRepository->createQueryBuilder('e');
         $qb->orderBy('e.id', 'desc');
 
+        $searchForm = $this->getSearchForm($request);
+
+        $this->applySearch($searchForm, $qb);
+
         return $this->render('@OHMediaBackend/email/email_index.html.twig', [
             'pagination' => $paginator->paginate($qb, 20),
             'view_attribute' => EmailVoter::VIEW,
+            'search_form' => $searchForm,
         ]);
+    }
+
+    private function getSearchForm(Request $request): FormInterface
+    {
+        $formBuilder = $this->container->get('form.factory')
+            ->createNamedBuilder('', FormType::class, null, [
+                'csrf_protection' => false,
+            ]);
+
+        $formBuilder->setMethod('GET');
+
+        $formBuilder->add('search', SearchType::class, [
+            'required' => false,
+            'label' => 'To, cc, bcc, from, subject, content',
+        ]);
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        return $form;
+    }
+
+    private function applySearch(FormInterface $form, QueryBuilder $qb): void
+    {
+        $search = $form->get('search')->getData();
+
+        if ($search) {
+            $searchFields = [
+                'e.to',
+                'e.cc',
+                'e.bcc',
+                'e.from',
+                'e.subject',
+                'e.html',
+            ];
+
+            $searchLikes = [];
+            foreach ($searchFields as $searchField) {
+                $searchLikes[] = "$searchField LIKE :search";
+            }
+
+            $qb->andWhere('('.implode(' OR ', $searchLikes).')')
+                ->setParameter('search', '%'.$search.'%');
+        }
     }
 
     #[Route('/email/{id}', name: 'email_view', methods: ['GET'])]
