e34893372a8523081408d417bdc99e295722feb8
Justin Hoffman <justin@ohmedia.ca>
2026-01-13 16:18:45 -0600

    ditch the alias

--- src/DependencyInjection/Compiler/NavPass.php
+++ src/DependencyInjection/Compiler/NavPass.php
@@ -2,6 +2,7 @@
 
 namespace OHMedia\BackendBundle\DependencyInjection\Compiler;
 
+use OHMedia\BackendBundle\Service\NavManager;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -11,11 +12,11 @@ class NavPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         // always first check if the primary service is defined
-        if (!$container->has('oh_media_backend.nav_manager')) {
+        if (!$container->has(NavManager::class)) {
             return;
         }
 
-        $definition = $container->findDefinition('oh_media_backend.nav_manager');
+        $definition = $container->findDefinition(NavManager::class);
 
         $tagged = $container->findTaggedServiceIds('oh_media_backend.nav_item_provider');
 
